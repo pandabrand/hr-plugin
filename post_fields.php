a24a57971bafaaa9a4bb6bbfdd92cc7e9b5b1741
pandabrand <pandabrand@gmail.com>
2020-06-09 18:04:23 -0500

    new field

--- post_fields.php
+++ post_fields.php
@@ -411,4 +411,52 @@ if( function_exists('acf_add_local_field_group') ):
 		'description' => '',
 	));
 	
+	acf_add_local_field_group(array(
+		'key' => 'group_5ee014688bb94',
+		'title' => 'images',
+		'fields' => array(
+			array(
+				'key' => 'field_5ee01477f5cff',
+				'label' => 'Background Image',
+				'name' => 'background_image',
+				'type' => 'image',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'return_format' => 'id',
+				'preview_size' => 'medium',
+				'library' => 'all',
+				'min_width' => '',
+				'min_height' => '',
+				'min_size' => '',
+				'max_width' => '',
+				'max_height' => '',
+				'max_size' => '',
+				'mime_types' => '',
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'city',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'side',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+	));
+	
 	endif;
